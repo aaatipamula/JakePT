f9689564f402e5a2d5305dcd4ad7057c1d4f4cfd
Omar Mohammed <ormohammed14@gmail.com>
2023-11-05 05:48:37 -0600

    cleanup

--- jakept/api.py
+++ jakept/api.py
@@ -3,5 +3,18 @@ Authors: Aniketh Aatipamula, Omar Mohammed
 Purpose: Set up the API
 '''
 from main import app
+from flask import requests
+import json 
 
-print('hello from api')
+file = open('data.json')
+database = json.load(file)
+
+
+
+@app.get("/api/info")
+def get_containers():
+    keys = requests.args.get()
+    keys_list = keys.split()
+    for item in keys_list:
+        if item in database:
+            return item
